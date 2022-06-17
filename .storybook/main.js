74839356206f8c10ec2633d0004d9f8b0f9556cc
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-06-17 09:00:21 -0300

    fix: fix story name pattern in main.js so stories appear in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,12 @@
 module.exports = {
   staticDirs: ['../public'],
-  stories: ['../src/components/**/stories.tsx'],
+  stories: ['../src/components/**/*.stories.tsx'],
   addons: ['@storybook/addon-essentials', 'storybook-addon-next-router'],
   core: {
-    builder: 'webpack5'
+    builder: 'webpack5',
   },
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
-  }
+  },
 }
