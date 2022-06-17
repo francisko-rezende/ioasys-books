5facb440e8c12bb96eba7de2fc3a44a5eb52a325
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-06-17 09:08:03 -0300

    feat: create theme file

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+  colors: {
+    white: '#fff',
+    gray: '#999',
+    black: '#333',
+    magenta: '#AB2680',
+  },
+  fontFamily:
+    "'Heebo', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+  fontWeights: {
+    thin: 300,
+    regular: 400,
+    thick: 500,
+  },
+}
