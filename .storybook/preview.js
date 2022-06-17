acb45768d85281c157c47181d326eebe13816c32
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-06-17 09:01:09 -0300

    fix: make storybook decorator use theme provider so stories have acess to theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,20 @@
 import { RouterContext } from 'next/dist/shared/lib/router-context'
+import { ThemeProvider } from 'styled-components'
+import theme from 'styles/theme.ts'
+
 import GlobalStyles from '../src/styles/global'
 
 export const parameters = {
   nextRouter: {
-    Provider: RouterContext.Provider
-  }
+    Provider: RouterContext.Provider,
+  },
 }
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
-  )
+    </ThemeProvider>
+  ),
 ]
