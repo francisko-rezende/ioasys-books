0810f6883bfdabc06825dfa23e1fd230a4f72a36
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-06-20 09:19:59 -0300

    feat: make inputs receive state updater fns and value

--- src/components/LoginInput/LoginInput.tsx
+++ src/components/LoginInput/LoginInput.tsx
@@ -7,11 +7,15 @@ import * as S from './LoginInput.styles'
 export type LoginInputProps = {
   inputName?: 'Senha' | 'Email'
   hasButton?: boolean
+  value?: string
+  updateValue: React.Dispatch<React.SetStateAction<string>>
 }
 
 const LoginInput = ({
   inputName = 'Senha',
   hasButton = true,
+  value,
+  updateValue,
 }: LoginInputProps) => {
   const inputType = inputName === 'Senha' ? 'password' : 'email'
   const placeholder =
@@ -21,7 +25,13 @@ const LoginInput = ({
     <S.Wrapper>
       <S.LabelWrapper>
         <S.Label htmlFor={inputName}>{inputName}</S.Label>
-        <S.Input id={inputName} placeholder={placeholder} type={inputType} />
+        <S.Input
+          id={inputName}
+          placeholder={placeholder}
+          type={inputType}
+          value={value}
+          onChange={(e) => updateValue(e.target.value)}
+        />
       </S.LabelWrapper>
       {hasButton && <LoginButton />}
     </S.Wrapper>
