05ba975fe25d75264f2f3f1691d428f56e43ed15
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-06-20 09:24:34 -0300

    feat: create first version of LoginPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
 
+import BackgroundImg from 'components/BackgroundImg'
+import LoginForm from 'components/LoginForm'
 import Logo from 'components/Logo'
-import Link from 'next/link'
+import MainContainer from 'components/MainContainer'
 
 const index = () => {
   return (
-    <div>
-      <h1>
-        <Logo /> Books
-      </h1>
-
-      <Link href="/home">
-        <a>Home</a>
-      </Link>
-    </div>
+    <>
+      <BackgroundImg src="/img/background-img-squoosh.jpg" />
+      <MainContainer>
+        <h1 style={{ paddingTop: '275px' }}>
+          <Logo color="white" />
+        </h1>
+        <LoginForm />
+      </MainContainer>
+    </>
   )
 }
 
