b3c22d0d0ea8e280074dab7bf90201722230c189
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-06-21 15:27:28 -0300

    feat: add background svg and logo to home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
 
 import BackgroundImg from 'components/BackgroundImg'
+import Logo from 'components/Logo'
+import MainContainer from 'components/MainContainer'
 import Link from 'next/link'
 
 const home = () => {
+  // const user = localStorage.getItem('user')
+  // console.log(user)
   return (
     <>
       <BackgroundImg src="/img/bg-home.svg" />
-      <h1>Página home</h1>
-      <Link href="/">
-        <a>Login</a>
-      </Link>
+      <MainContainer>
+        <h1>
+          <Logo color="black" />
+        </h1>
+        <Link href="/">
+          <a>Login</a>
+        </Link>
+      </MainContainer>
     </>
   )
 }
