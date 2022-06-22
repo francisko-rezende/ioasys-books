06db405cdf6dff1b04cfc2173c2d8d2501641668
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-06-22 10:15:08 -0300

    feat: try using native HTML typing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,66 @@
 import React from 'react'
 
-import Logo from 'components/Logo'
 import Link from 'next/link'
 
+interface childrenTestProps {
+  children: React.ReactNode
+  // children: React.ReactElement
+}
+
+const ChildrenTest = ({ children }: childrenTestProps) => {
+  return <div>{children}</div>
+}
+
 const index = () => {
+  // const mockData = [
+  //   {
+  //     id: 1,
+  //     title: 'foo',
+  //     body: 'bar',
+  //     userId: 1,
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'foo',
+  //     body: 'bar',
+  //     userId: 2,
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'foo',
+  //     body: 'bar',
+  //     userId: 3,
+  //   },
+  //   {
+  //     id: 4,
+  //     title: 'foo',
+  //     body: 'bar',
+  //     userId: 4,
+  //   },
+  //   {
+  //     id: 5,
+  //     title: 'foo',
+  //     body: 'bar',
+  //     userId: 5,
+  //   },
+  // ]
   return (
-    <div>
-      <h1>
-        <Logo /> Books
-      </h1>
+    <>
+      <h1>Ioasys Books</h1>
 
       <Link href="/home">
         <a>Home</a>
       </Link>
-    </div>
+      {/* {mockData.map((item) => (
+        <ChildrenTest key={item.id}>
+          <div>{item.title}</div>
+        </ChildrenTest>
+      ))} */}
+      <ChildrenTest>
+        <div>Alo alo</div>
+        <div>Graças a deus</div>
+      </ChildrenTest>
+    </>
   )
 }
 
