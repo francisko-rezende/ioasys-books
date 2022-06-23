d1ae2d5665a6195f4a923b1151929888ca9d376b
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-06-23 11:14:51 -0300

    refactor: make BookCard receive whole book obj as a prop

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -1,37 +1,41 @@
 import React from 'react'
 
+import { Book } from 'components/BookList/BookList'
 import Image from 'next/image'
 
 import * as S from './BookCard.styles'
 
-interface BookCardProps {
-  bookTitle: string
-  author: string
-  pageCount: number
-  publisher: string
-  published: number
-  bookCover?: string
+type BookCardProps = {
+  book: Book
 }
 
-const BookCard = ({
-  bookTitle = 'Crossing the Chasm',
-  author = 'Geoffrey A. Moore',
-  pageCount = 150,
-  publisher = 'Editora Loyola',
-  published = 2020,
-  bookCover,
-}: BookCardProps) => {
+const BookCard = ({ book }: BookCardProps) => {
+  const { authors, title, pageCount, published, publisher, imageUrl } = book
+
+  const renderAuthors = (authors: string[]) =>
+    authors.length <= 2 ? (
+      authors.map((author) => <S.Author key={author}>{author}</S.Author>)
+    ) : (
+      <S.Author>
+        {authors[0]} <em>et al</em>
+      </S.Author>
+    )
+
   return (
     <S.Wrapper>
       <Image
-        src={bookCover ? bookCover : '/img/book-cover-placeholder.webp'}
+        src={imageUrl ? imageUrl : '/img/book-cover-placeholder.webp'}
         width={81}
         height={122}
       />
       <S.BookInfoWrapper>
         <div>
-          <S.BookTitle>{bookTitle}</S.BookTitle>
-          <S.Author>{author}</S.Author>
+          <S.BookTitle>{title}</S.BookTitle>
+          {authors ? (
+            renderAuthors(authors)
+          ) : (
+            <S.Author>Autor desconhecido</S.Author>
+          )}
         </div>
         <S.BookDetails>
           <li>{`${pageCount} páginas`}</li>
